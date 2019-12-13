6ac5ed5b9d51921b846cb58d1d2492b952020db0
alepareyes <alejandra.reyesg96@gmail.com>
2019-12-13 11:42:39 +0100

    refactor whole js

--- _src/assets/js/03-share.js
+++ _src/assets/js/03-share.js
@@ -1,3 +1,8 @@
+const createAllowCard = document.querySelector('.js-share__btn');
+const formValidation = document.querySelector('.js-form-container');
+
+
+
 function checkForm() {
   if (formValidation.checkValidity() === true) {
     createAllowCard.classList.remove('filter');
@@ -5,3 +10,15 @@ function checkForm() {
     createAllowCard.classList.add('filter');
   }
 }
+
+const twitterActive = function () {
+  if (formValidation.checkValidity() === false) {
+    twitterBtn.classList.add('hidden');
+  }
+};
+
+function createButtonHandler(event) {
+  event.preventDefault();
+  twitterActive();
+}
+createAllowCard.addEventListener('click', createButtonHandler);
--- _src/assets/js/04-image.js
+++ _src/assets/js/04-image.js
@@ -1,3 +1,13 @@
+
+const fr = new FileReader();
+const uploadBtn = document.querySelector('.js-fill__image-btn');
+const fileField = document.querySelector('.js-fill__image-upload-btn');
+const profileImage = document.querySelector('.preview__display--img');
+const profilePreview = document.querySelector('.fill__image--miniature');
+const resetFormBtn = document.querySelector('.preview__reset');
+const defaultImage = './assets/images/default-profile.png';
+
+
 function getImage(e) {
   var myFile = e.currentTarget.files[0];
   fr.addEventListener('load', writeImage);
@@ -21,3 +31,6 @@ function fakeFileClick(e) {
   e.preventDefault();
   fileField.click();
 }
+
+uploadBtn.addEventListener('click', fakeFileClick);
+fileField.addEventListener('change', getImage);
