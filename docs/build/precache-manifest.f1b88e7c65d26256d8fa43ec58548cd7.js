c68ee0d887a537713ecc750aabd285123dab716d
Saralopezlovon <lopezlovonsara@gmail.com>
2019-12-17 11:31:20 +0100

    Update docs

---
+++ docs/build/precache-manifest.f1b88e7c65d26256d8fa43ec58548cd7.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "093cf7eec541b29ed4aa4171a9e8571c",
+    "url": "./index.html"
+  },
+  {
+    "revision": "0f0f5e39889a9ee7c454",
+    "url": "./static/css/main.76a71478.chunk.css"
+  },
+  {
+    "revision": "03a84112b01cd25e5928",
+    "url": "./static/js/2.1ee8c69e.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "./static/js/2.1ee8c69e.chunk.js.LICENSE"
+  },
+  {
+    "revision": "0f0f5e39889a9ee7c454",
+    "url": "./static/js/main.45ccc73c.chunk.js"
+  },
+  {
+    "revision": "109e03da64b625e17797",
+    "url": "./static/js/runtime-main.6ad335e0.js"
+  },
+  {
+    "revision": "7ca42afb4685f49f6bdfef6e31724e16",
+    "url": "./static/media/ECOME.7ca42afb.png"
+  },
+  {
+    "revision": "20a16716dbe773c50c18a98a68446384",
+    "url": "./static/media/default-profile.20a16716.png"
+  },
+  {
+    "revision": "4fa6ba4c7d99d9924230910b4270dffb",
+    "url": "./static/media/flower.4fa6ba4c.jpg"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "./static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "0118e311c7fc2f113bee1abedecf92b2",
+    "url": "./static/media/logoheader.0118e311.svg"
+  }
+]);
