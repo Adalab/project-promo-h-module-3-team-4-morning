e24aad7338695205357f924b87a27affd960d160
Saralopezlovon <lopezlovonsara@gmail.com>
2019-12-18 15:48:24 +0100

    create Input component adapted to form

--- src/components/Fill.js
+++ src/components/Fill.js
@@ -1,18 +1,15 @@
 import React from "react";
 import "../stylesheets/mainStyle.scss";
+import Input from "./Input";
 
 class Fill extends React.Component {
   render() {
     return (
       <fieldset className="fill hidden">
-        <label className="fill__lable" htmlFor="name">
-          Nombre completo
-        </label>
-        <input className="fill__box js-input" type="text" id="name" name="name" placeholder="Ej: Sally Jill" required />
-        <label className="fill__lable " htmlFor="job">
-          Puesto
-        </label>
-        <input className="fill__box js-input" type="text" id="job" name="job" placeholder="Ej: Front-end unicorn" />
+        <Input htmlFor="name" type="text" text="Nombre completo" id="name" name="name" placeholder="Ej: Sally Jill" />
+
+        <Input htmlFor="job" type="text" text="Puesto" id="job" name="job" placeholder="Ej: Front-end unicorn" />
+
         <label className="fill__lable" htmlFor="image">
           Imagen de perfil
         </label>
@@ -21,22 +18,14 @@ class Fill extends React.Component {
           <input type="file" name="" id="img-selector" className="hidden js-fill__image-upload-btn" />
           <div className="fill__image--miniature"></div>
         </div>
-        <label className="fill__lable" htmlFor="email">
-          Email
-        </label>
-        <input className="fill__box js-input" type="email" id="email" name="email" placeholder="Ej: sally-hill@gmail.com" required />
-        <label className="fill__lable" htmlFor="phone">
-          Teléfono
-        </label>
-        <input className="fill__box js-input" type="tel" id="phone" name="phone" placeholder="Ej: 555-55-55-55" pattern="[0-9]{9}" required />
-        <label className="fill__lable" htmlFor="linkedin">
-          Linkedin
-        </label>
-        <input className="fill__box js-input" type="text" id="linkedin" name="linkedin" placeholder="Ej: sally.hill" required />
-        <label className="fill__lable" htmlFor="github">
-          Github
-        </label>
-        <input className="fill__box js-input" type="text" id="github" name="github" placeholder="Ej: @sally-hill" required />
+
+        <Input htmlFor="email" type="email" text="Email" id="email" name="email" placeholder="Ej: sally-hill@gmail.com" />
+
+        <Input htmlFor="phone" type="tel" text="Teléfono" id="phone" name="phone" placeholder="Ej: 555-55-55-55" pattern="[0-9]{9}" />
+
+        <Input htmlFor="linkedin" type="text" text="Linkedin" id="linkedin" name="linkedin" placeholder="Ej: sally.hill" />
+
+        <Input htmlFor="github" type="text" text="Github" id="github" name="github" placeholder="Ej: @sally-hill" />
       </fieldset>
     );
   }
---
+++ src/components/Input.js
@@ -0,0 +1,17 @@
+import React from "react";
+import "../stylesheets/mainStyle.scss";
+
+class Input extends React.Component {
+  render() {
+    return (
+      <div>
+        <label className="fill__lable" htmlFor={this.props.htmlFor}>
+          {this.props.text}
+        </label>
+        <input className="fill__box js-input" type={this.props.type} id={this.props.id} name={this.props.name} placeholder={this.props.placeholder} pattern={this.props.pattern} required />
+      </div>
+    );
+  }
+}
+
+export default Input;
