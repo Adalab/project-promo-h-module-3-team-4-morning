ad90a71a81fe5d5727fc161f09f6028b5558836a
Saralopezlovon <lopezlovonsara@gmail.com>
2019-12-22 20:53:25 +0100

    change on Preview

--- src/components/Icon.js
+++ src/components/Icon.js
@@ -4,8 +4,8 @@ import "../stylesheets/mainStyle.scss";
 class Icon extends React.Component {
   render() {
     return (
-      <li className={this.props.classLi}>
-        <a className={this.props.classA} href="" target="_blank">
+      <li className={`menu__items ${this.props.classLi}`}>
+        <a className={`menu__links ${this.props.classA}`} href="" target="_blank">
           <i className={this.props.classI}></i>
         </a>
       </li>
