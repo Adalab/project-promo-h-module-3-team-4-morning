4ad223c31759ec22797af6d70876d2dd68c1d59b
alepareyes <alejandra.reyesg96@gmail.com>
2019-12-23 06:58:29 +0100

    add htmlFor value to id and for

--- src/components/Fill.js
+++ src/components/Fill.js
@@ -6,8 +6,8 @@ class Fill extends React.Component {
   render() {
     return (
       <fieldset className="fill">
-        <Input htmlFor="name" type="text" text="Nombre completo" id="name" name="name" placeholder="Ej: Sally Jill" />
-        <Input htmlFor="job" type="text" text="Puesto" id="job" name="job" placeholder="Ej: Front-end unicorn" />
+        <Input htmlFor="name" type="text" text="Nombre completo" name="name" placeholder="Ej: Sally Jill" />
+        <Input htmlFor="job" type="text" text="Puesto" name="job" placeholder="Ej: Front-end unicorn" />
         <label className="fill__lable" htmlFor="image">
           Imagen de perfil
         </label>
@@ -16,10 +16,10 @@ class Fill extends React.Component {
           <input type="file" name="" id="img-selector" className="hidden js-fill__image-upload-btn" />
           <div className="fill__image--miniature"></div>
         </div>
-        <Input htmlFor="email" type="email" text="Email" id="email" name="email" placeholder="Ej: sally-hill@gmail.com" />
-        <Input htmlFor="phone" type="tel" text="Teléfono" id="phone" name="phone" placeholder="Ej: 555-55-55-55" pattern="[0-9]{9}" />
-        <Input htmlFor="linkedin" type="text" text="Linkedin" id="linkedin" name="linkedin" placeholder="Ej: sally.hill" />
-        <Input htmlFor="github" type="text" text="Github" id="github" name="github" placeholder="Ej: @sally-hill" />
+        <Input htmlFor="email" type="email" text="Email" name="email" placeholder="Ej: sally-hill@gmail.com" />
+        <Input htmlFor="phone" type="tel" text="Teléfono" name="phone" placeholder="Ej: 555-55-55-55" pattern="[0-9]{9}" />
+        <Input htmlFor="linkedin" type="text" text="Linkedin" name="linkedin" placeholder="Ej: sally.hill" />
+        <Input htmlFor="github" type="text" text="Github" name="github" placeholder="Ej: @sally-hill" />
       </fieldset>
     );
   }
