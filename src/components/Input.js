a1d8845ac51ab02ca28aa8f8dd99eef306150e18
Beatriz Sobrino Paredes <Beatrizsobri@github.com>
2019-12-27 17:12:09 +0100

    add input feature - in progress

--- src/components/Input.js
+++ src/components/Input.js
@@ -2,36 +2,6 @@ import React from "react";
 import "../stylesheets/input.scss";
 
 class Input extends React.Component {
-  constructor(props) {
-    super(props);
-    // this.state = {
-    //   name: '',
-    //   job: '',
-    //   email: '',
-    //   phone: '',
-    //   linkedin: '',
-    //   github: ''
-    // }
-    this.handleInputValue = this.handleInputValue.bind(this);
-  }
-
-  handleInputValue(ev) {
-    // debugger;
-    // console.log(`esto es name` + this.state.name)
-    // console.log('esto es job' + this.state.job)
-    // if (ev.currentTarget.id === 'name') {
-    //   this.setState({
-    //     name: ev.currentTarget.value
-    //   });
-    // }
-
-    this.props.handleChange({
-      id: this.props.id,
-      value: ev.currentTarget.value
-    })
-
-  }
-
 
   render() {
     return (
@@ -39,7 +9,8 @@ class Input extends React.Component {
         <label className="fill__lable" htmlFor={this.props.htmlFor}>
           {this.props.text}
         </label>
-        <input onChange={this.handleInputValue} className="fill__box js-input" type={this.props.type} id={this.props.id} name={this.props.name} placeholder={this.props.placeholder} pattern={this.props.pattern} required />
+        <input onChange={this.props.handleChange} className="fill__box js-input" type={this.props.type} id={this.props.id} name={this.props.name} placeholder={this.props.placeholder} pattern={this.props.pattern} value={this.props.value} required />
+        <p>El value es: {this.props.value}</p>
       </div>
     );
   }
