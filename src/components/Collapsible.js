988df600a915ffa4c54b8d5fe81f8b0651143005
Sandra Romero <romeroortiz.sandra@gmail.com>
2020-01-07 11:19:22 +0100

    Go on with collapsibles

--- src/components/Collapsible.js
+++ src/components/Collapsible.js
@@ -1,19 +1,32 @@
 import React from "react";
 import "../stylesheets/collapsibles.scss";
 
-
 class Collapsible extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      closed: true,
+    };
+    this.handleClose = this.handleClose.bind(this);
+  }
+
+  handleClose() {
+    console.log("abrir y cerrar", this.state);
+    // this.setState({ closed: ev.target.prevState });
+  }
+
   render() {
     return (
       <div>
-        <legend className="collapsible">
+        <legend className="collapsible" onClick={this.handleClose}>
           <div className="collapsible__left">
             <i className={`collapsible__left--fav ${this.props.classIcon}`}></i>
             <h2 className="collapsible__left--title">{this.props.title}</h2>
           </div>
-          <i className="collapsible--arrow fas fa-angle-down"></i>
+          <i className="collapsible--arrow fas fa-angle-down "></i>
         </legend>
-        {this.props.children}
+        <div className="collapsibleWindow closed">{this.props.children}</div>
       </div>
     );
   }
