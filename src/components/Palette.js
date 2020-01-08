6d2d8d01920bfe66ccd04d775a92870409cc1f9d
Beatriz Sobrino Paredes <Beatrizsobri@github.com>
2020-01-08 12:46:50 +0100

    Amend defaultChecked

--- src/components/Palette.js
+++ src/components/Palette.js
@@ -7,7 +7,7 @@ class Palette extends React.Component {
     return (
       <div>
         <label className="design-box__theme" htmlFor={this.props.htmlFor}>
-          <input onClick={this.props.paletteHandler} id={this.props.htmlFor} type="radio" checked={this.props.checked} value={this.props.value} name="palette" />
+          <input onClick={this.props.paletteHandler} id={this.props.htmlFor} type="radio" defaultChecked={this.props.defaultChecked} value={this.props.value} name="palette" />
           <div className="colors__container">
             <div className={this.props.color1}></div>
             <div className={this.props.color2}></div>
