bec49c7ed31dd8854361b91bf7c867886433a4fd
Beatriz Sobrino Paredes <Beatrizsobri@github.com>
2020-01-09 13:04:53 +0100

    add LS feature, still pending to solve conflict

--- src/index.js
+++ src/index.js
@@ -3,8 +3,6 @@ import ReactDOM from "react-dom";
 import { HashRouter } from "react-router-dom";
 import App from "./components/App";
 
-// import Landing from "./components/Landing";
-
 ReactDOM.render(
   <HashRouter>
     <App />
