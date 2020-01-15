0cd3c65a91cc456609c71abee07aa7e6bc8f2097
Beatriz Sobrino Paredes <Beatrizsobri@github.com>
2020-01-15 17:17:53 +0100

    Polish Fetch

---
+++ src/services/Api.js
@@ -0,0 +1,13 @@
+const createCardApi = 'https://us-central1-awesome-cards-cf6f0.cloudfunctions.net/card/';
+
+const createCardFetch = (json) =>
+    fetch(createCardApi, {
+        method: 'POST',
+        body: JSON.stringify(json),
+        headers: {
+            'content-type': 'application/json'
+        },
+    })
+        .then(function (resp) { return resp.json(); })
+
+export { createCardFetch };
